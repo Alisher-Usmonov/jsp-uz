dd4fc577cb179c51a7edd2e0f1d25dcd2ba627cc
Alisher-Usmonov <alisherufullstack@gmail.com>
2021-10-22 06:48:40 +0500

    api

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    "Access-Control-Allow-Origin": "*"
+}));
 
 // Routes
 fs.readdir(path.join(__dirname, "routes"), "utf-8", (err, files) => {
